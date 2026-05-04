2afbbec7d6c11f8cf9042dc9f73f58a122b824d9
manaramira2003-alt <amiramanar2003@gmail.com>
2026-05-04 18:13:21 +0200

    fix neon db config for production

--- config/database.js
+++ config/database.js
@@ -3,12 +3,13 @@ require("dotenv").config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  ssl: process.env.NODE_ENV === "production"
+  dialectOptions: {
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
+}
 });
 
-// test connection
-sequelize.authenticate()
-  .then(() => console.log("Database connected ✔"))
-  .catch(err => console.log("DB error ❌", err));
 
 module.exports = sequelize;
